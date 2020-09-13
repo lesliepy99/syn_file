a17357d08d46a6f46ac7a0de1fdd3773729cba52
lesliepy99 <1155124579@link.cuhk.edu.hk>
2020-09-13 11:01:14 +0800

    init

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require(express)
+var express = require('express')
 var app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
 
 var result_string = "nothing yet"
+app.get('/approach',(req,res)=>{
+    console.log('send')
+    res.send(result_string)
+})
+app.get('/*',function (req, res){
+    res.sendFile(__dirname+'/public/index.html')
+})
 app.post('/receive_text',(req,res)=>{
     result_string = req.body['content']
-    res.send('received')
+    res.send(result_string+"recieved")
 })
+
+
+const server = app.listen(3000)
