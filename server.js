ab73f82a7e97f4dfa7d6b67ed67f23efe934d278
lesliepy99 <1155124579@link.cuhk.edu.hk>
2020-09-13 23:33:08 +0800

    Add the function of uploading files

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var app = express()
+upload = require("express-fileupload")
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(upload())
+
+
 
 var result_string = "nothing yet"
 app.get('/approach',(req,res)=>{
@@ -15,5 +19,23 @@ app.post('/receive_text',(req,res)=>{
     result_string = req.body['content']
 })
 
+app.post('/upload_file',function (req, res){
+    console.log(req.files)
+    if(req.files){
+        var file = req.files.myfile
+        filename = file.name
+        file.mv("files/"+filename,function(err){
+            console.log(err)
+        }
+        
+        )
+    }
+    else{
+        console.log('error for files')
+    }
+
+    res.redirect('/')
+    
+})
 
 const server = app.listen(3000)
