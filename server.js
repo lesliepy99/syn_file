89424f7e4f95b653ed1e79050c0ea6e21a472baa
lesliepy99 <1155124579@link.cuhk.edu.hk>
2020-09-14 08:41:21 +0800

    return list of files in the directory

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var path = require('path')
+var fs = require('fs')
 upload = require("express-fileupload")
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
@@ -8,18 +10,45 @@ app.use(upload())
 
 
 var result_string = "nothing yet"
+var the_path = path.join(__dirname,"files")
+
+//Approach the text
 app.get('/approach',(req,res)=>{
     console.log('send')
     res.send(result_string)
 })
+
+//Show the list of files
+app.get('/fresh_list',function (req, res){
+    
+    fs.readdir(the_path,function(err,files){
+        if(err){
+            res.send('Error of getting the directory list')
+        }
+        
+        
+        else{
+            file_container = []
+            files.forEach(function(file){
+                file_container.push(file)
+            })
+            res.send(file_container)
+        }
+    })
+})
+
+//Get the HTML file
 app.get('/*',function (req, res){
     res.sendFile(__dirname+'/public/index.html')
 })
+
+//Upload text
 app.post('/receive_text',(req,res)=>{
     result_string = req.body['content']
     res.redirect('/')
 })
 
+//Upload files
 app.post('/upload_file',function (req, res){
     console.log(req.files)
     if(req.files){
@@ -38,5 +67,7 @@ app.post('/upload_file',function (req, res){
     res.redirect('/')
     
 })
+
+
 
 const server = app.listen(3000)
